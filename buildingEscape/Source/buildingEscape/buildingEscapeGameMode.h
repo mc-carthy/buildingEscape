8a76de3d129fb95cdb10034a70a2a432bcaec01a
mc-carthy <mccarthy.mick0012@gmail.com>
2016-10-10 11:20:30 +0100

    Init repo

---
+++ buildingEscape/Source/buildingEscape/buildingEscapeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "buildingEscapeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPE_API AbuildingEscapeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
